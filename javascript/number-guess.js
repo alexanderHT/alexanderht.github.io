4ccc229f7ba467955ae0f0f8c9776a2405a72d4f
alexanderHT <alexanderhendrawan@gmail.com>
2016-09-17 01:21:38 +0700

    update gess number

--- javascript/number-guess.js
+++ javascript/number-guess.js
@@ -5,27 +5,16 @@ var ronde = 1;
 // function global
 // function komputer untuk menghasilkan angka acak dari 1 sampai 10
 var komputer = function(){
-    var pilih = Math.random().toFixed(1);
-    switch (pilih) {
-      case '0.0': return 10; break;
-      case '0.1': return 1; break;
-      case '0.2': return 2; break;
-      case '0.3': return 3; break;
-      case '0.4': return 4; break;
-      case '0.5': return 5; break;
-      case '0.6': return 6; break;
-      case '0.7': return 7; break;
-      case '0.8': return 8; break;
-      case '0.9': return 9; break;
-      default: return 10; break;
-    }
+    return Math.floor((Math.random() * 10) + 1);
 }
 
 // program tebak
 do{
+  // varibale local
   var pilihanPengguna = Number(prompt("Masukan angka tebakan?"));
   var pilihanKomputer = komputer();
 
+  // keterangan
   console.log("====== RONDE " + ronde + " ======");
   console.log("pilihan Pengguna : " + pilihanPengguna);
   console.log("pilihan Komputer : " + pilihanKomputer);
