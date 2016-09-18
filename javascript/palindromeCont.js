af9e0f1f8dd5f15f87b88103a3cf74b0a234d82f
alexanderHT <alexanderhendrawan@gmail.com>
2016-09-19 01:56:26 +0700

    tugas week-3

--- javascript/palindromeCont.js
+++ javascript/palindromeCont.js
@@ -1,10 +1,19 @@
+// variable global
 var input = prompt("masukan sebuah kalimat?");
 
 var palindromeCont = function(input){
-  var kalimatAwal = input;
-  var array = [];
+  // variable local
+  var awal = input.split(" ");
+  var akhir = input.split(" ").sort();
 
-  
+  console.log(awal.join(" "));
+  console.log(akhir.join(" "));
 
-
+  if(awal.length = akhir.length){
+    console.log("benar");
+  }else{
+    console.log("salah");
+  }
 }
+
+palindromeCont(input);
