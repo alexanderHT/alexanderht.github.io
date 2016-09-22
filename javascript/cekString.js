efb24295f3dfd881b1405f5bbc0aa62975f33457
alexanderHT <alexanderhendrawan@gmail.com>
2016-09-23 03:47:16 +0700

    update

--- javascript/cekString.js
+++ javascript/cekString.js
@@ -1,14 +1,16 @@
-var input1 = "aaawossrld".split("").sort();
-var input2 = "worldd".split("").sort();
-console.log(input1);
-console.log(input2);
-
+// var input1 = "aaawossrld".split("").sort();
+// var input2 = "worldd".split("").sort();
+var input1 = prompt("masukan kata pertama ? ").split("").sort();
+var input2 = prompt("masukan kata kedua ? ").split("").sort();
+// console.log(input1);
+// console.log(input2);
 
 var cekString = function(input1, input2){
   i = 0;
   var status = true;
 
   while(status){
+
     console.log("i: ",i)
     console.log("input1: ",input1)
     console.log("input2: ",input2[i])
@@ -25,7 +27,7 @@ var cekString = function(input1, input2){
     i++;
   }
 
-  return status
+  return status;
 }
 
 console.log(cekString(input1, input2));
--- javascript/faktorPrima.js
+++ javascript/faktorPrima.js
@@ -3,7 +3,8 @@ var output = [];
 
 var faktor_prima = function(input){
   var pangkat = 1;
-  // looping untuk memasukan faktor bilangan prima kedalam varibale output
+  // looping untuk memasukan faktor bilangan prima kedalam varibale output,
+  // dimana variable output menyimpan array yang berisi faktor prima dari angka yang di inputkan
   for(var i = 2; i <= input; i++){
     while(input % i === 0 ){
       input = input / i;
@@ -16,10 +17,14 @@ var faktor_prima = function(input){
 
   // looping untuk menghitung pangkat
   for(var j = 0; j < output.length; j++){
+    // membandingkan jika output dengan index ke 0 dan seterusnya dicek / di bandingkan dengan index berikutnya,
+    // jika sama maka setiap pangkat di tambah
     if(output[j] === output[j+1]){
       pangkat ++;
     }else{
+      // jika hasil dari index sebelum dan sesudahnya berbeda maka cetak hasilnya berserta pangkatnya
       console.log(output[j] + " pangkat : " + pangkat);
+      // reset pangkat menjadi satu kembali
       pangkat = 1;
     }
   }
